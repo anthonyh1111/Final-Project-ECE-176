017c3d90cc50ab8239504da66f43f5662c562d6f
anthonyh1111 <anthonyx1948@gmail.com>
2026-03-08 18:11:30 -0700

    Update cnn.py

--- models/cnn.py
+++ models/cnn.py
@@ -4,33 +4,34 @@ import torch.nn.functional as F
 
 
 class SmallCNN(nn.Module):
-    def __init__(self):
-        super(SmallCNN, self).__init__()
+    def __init__(self, num_classes=10):
+        super().__init__()
 
-        self.conv1 = nn.Conv2d(3, 32, 3, padding=1)
-        self.conv2 = nn.Conv2d(32, 32, 3, padding=1)
+        self.conv1 = nn.Conv2d(3, 32, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(32, 32, kernel_size=3, padding=1)
 
-        self.conv3 = nn.Conv2d(32, 64, 3, padding=1)
-        self.conv4 = nn.Conv2d(64, 64, 3, padding=1)
+        self.conv3 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
+        self.conv4 = nn.Conv2d(64, 64, kernel_size=3, padding=1)
 
-        self.pool = nn.MaxPool2d(2, 2)
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
 
         self.fc1 = nn.Linear(64 * 8 * 8, 128)
-        self.fc2 = nn.Linear(128, 10)
+        self.dropout = nn.Dropout(p=0.3)
+        self.fc2 = nn.Linear(128, num_classes)
 
     def forward(self, x):
-
         x = F.relu(self.conv1(x))
         x = F.relu(self.conv2(x))
-        x = self.pool(x)
+        x = self.pool(x)  # 32x32 -> 16x16
 
         x = F.relu(self.conv3(x))
         x = F.relu(self.conv4(x))
-        x = self.pool(x)
+        x = self.pool(x)  # 16x16 -> 8x8
 
-        x = x.view(x.size(0), -1)
+        x = torch.flatten(x, start_dim=1)
 
         x = F.relu(self.fc1(x))
+        x = self.dropout(x)
         x = self.fc2(x)
 
         return x
